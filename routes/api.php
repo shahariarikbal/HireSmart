58ef52532632343ce134725ec4198f81290cdfbd
shahariarikbal <shahariar.ikbal86@gmail.com>
2025-06-26 22:17:29 +0600

    feat:Create Restricted role middleware and implement

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (){
     Route::post('login', [AuthController::class, 'login'])->name('login')->middleware('throttle:5,1');
-    
+
     Route::middleware('auth:api')->group(function () {
+        Route::get('/admin/dashboard', [AuthController::class, 'admin_dashbaord'])->middleware('role:admin');
+        Route::get('/employer/dashboard', [AuthController::class, 'employer_dashbaord'])->middleware('role:employer');
+        Route::get('/candidate/dashboard', [AuthController::class, 'candidate_dashbaord'])->middleware('role:candidate');
         Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
